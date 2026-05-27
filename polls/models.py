cc108de0e5987ada9d8b138ed1b1e5ef502499e3
jPadua371 <joaopadua43432@gmail.com>
2026-05-27 11:26:53 -0300

    feat: finishing part 4

--- polls/models.py
+++ polls/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
-
-class Question(models.Model):
+class Question(models.Model):  
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -13,8 +12,7 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-
-class Choice(models.Model):
+class Choice(models.Model):  # ← Choice depois de Question
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
